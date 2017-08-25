764a6af19d4f0e61bd3bd5504a4fa9a5ce604dce
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2017-08-25 14:41:29 -0700

    Paso de parametros GET
    ...

---
+++ Node7/app.js
@@ -0,0 +1,97 @@
+//Modulos
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var querystring = require("querystring");
+
+
+var mime = {
+     'html' : 'text/html',
+     'css' : 'text/css',
+     'js' : 'text/javascript',
+     'png' : 'image/png',
+     'mov' : 'video/mov',
+     'm4a' : 'audio/m4a'
+};
+
+
+var servidor = http.createServer(function(request,response){
+
+    var objUrl = url.parse(request.url);
+
+    var path =  'pages'+objUrl.pathname;
+
+    console.log(path);
+
+    if (path == 'pages/') {
+        path = 'pages/index.html';
+    }else if (path == "pages/tablaMultiplicar"){
+      tablaMultiplicar(request, response);
+      return;
+    }
+  
+
+    //Verificacion de que si esta el archivo en la ruta 
+    fs.exists(path, function(existe){
+
+         if (existe) {
+
+         	fs.readFile(path, function(error,contenido){
+                 
+                 if (error) {
+
+                 	response.writeHead(500,{'Content-Type':'text/plain'});
+                 	response.write('Error interno');
+                 	response.end();
+
+                 }else{
+                     
+                     //Se define el tipo de dato cargado
+
+                     //console.log(path);
+
+                     var  datosSeparados = path.split('.');
+                     //console.log(datosSeparados);
+
+                     var extension =   datosSeparados[datosSeparados.length - 1]; //.html , .mov, .css
+
+                     var mimeArch = mime[extension];
+
+                     console.log("Mi estension es: "+mimeArch);
+
+                 	response.writeHead(200,{'Content-Type':mimeArch});
+                 	response.write(contenido);
+                 	response.end();
+                 }
+         	});
+         }else{
+         	response.writeHead(404,{'Content-Type':'text/html'});
+         	response.write('<html><head><body>Recurso no existe 404</body></head></html>');
+         	response.end();
+         }
+    });
+    
+
+});
+
+function tablaMultiplicar(request, response){
+    var  informacion = "";
+
+    var tablaAMultiplicar  = url.parse(request.url,true).query.numero;
+    console.log(tablaAMultiplicar);
+    var pagina = "<html><head><body>";
+          for (var i = 1; i <= 10; i++) {
+              pagina += ""+tablaAMultiplicar+"x"+i+"="+(tablaAMultiplicar * i)+"<br/>";
+          }
+         pagina += "</body></head></html>";
+
+     response.writeHead(200,{"content-type":"text/html"});
+     response.end(pagina);
+
+}
+
+
+servidor.listen(8000);
+
+console.log("Servidor Web Iniciado");
